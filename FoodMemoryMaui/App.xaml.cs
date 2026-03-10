d20a6aa65723486c91fd7e3c361ceb6b1ed83956
Mishel Dekin <mishel.dekin@gmail.com>
2026-03-10 14:30:43 +0200

    sgdrbe

---
+++ FoodMemoryMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FoodMemoryMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ FoodMemoryMaui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace FoodMemoryMaui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
